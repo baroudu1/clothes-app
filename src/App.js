bf6523c6073f1986a7bd2827bab88d23528e81e9
Hamza BAROUDI <61873638+baroudu1@users.noreply.github.com>
2022-06-26 10:53:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -8,15 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        </p>    
           Learn React
-        </a>
       </header>
     </div>
   );
